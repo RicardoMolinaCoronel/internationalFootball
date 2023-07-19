15db4932533bec7dd05b0e2556c4d1201cff3087
Cliente CONTROL-PC <CltControl@GGIECLUGWRK169.espol.edu.ec>
2023-07-19 14:50:41 -0500

    push

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,5 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+ //Importe el módulo cliente para requerimientos http
+ import { HttpClientModule } from '@angular/common/http';
+
+ //Importe el servicio
+ import { ServicioService } from '../providers/servicio.service';
+
+
 import { AboutComponent } from './about.component';
 
 describe('AboutComponent', () => {
@@ -8,7 +15,13 @@ describe('AboutComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AboutComponent]
+      declarations: [AboutComponent],
+
+              //Registre el módulo de petición http
+              imports: [ HttpClientModule ],
+
+              //Registre el servicio como proveedor de datos
+              providers: [ ServicioService ]
     });
     fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
@@ -18,4 +31,25 @@ describe('AboutComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should request service user after Angular calls ngOnInit', (done: DoneFn) => {
+
+    // Llame a ngOnInit para simular el ciclo de vida del componente
+    component.ngOnInit();
+
+
+    // Utilice fixture.whenStable para esperar a que se resuelva el observable del servicio
+    fixture.whenStable().then(() => {
+      
+
+      // Valide que la respuesta sea mayor que 0
+      expect(component.data.length).toBeGreaterThan(0)
+
+      // Que espere hasta que llegue la respuesta
+      done();
+
+    });
+  });
+
+
 });
--- src/app/providers/servicio.service.spec.ts
+++ src/app/providers/servicio.service.spec.ts
@@ -2,15 +2,41 @@ import { TestBed } from '@angular/core/testing';
 
 import { ServicioService } from './servicio.service';
 
+import { HttpClientModule } from '@angular/common/http';
+
+  //Importe la interfaz
+import { Persona } from '../interfaces/persona';
+
 describe('ServicioService', () => {
   let service: ServicioService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+              //Registre el módulo de petición http
+              imports: [ HttpClientModule ],
+              //Registre el servicio como proveedor de datos
+              providers: [ ServicioService ]
+      
+    });
     service = TestBed.inject(ServicioService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+    // La función done se encarga de esperar por completar el requerimiento
+    it('observable_t should return value from observable', (done: DoneFn) => {
+    	//Invoque el método con la petición asincrónica
+      service.getResponse().subscribe(data => {
+	      
+        // Valide que la respuesta sea mayor que 0
+        expect((data as Persona[]).length).toBeGreaterThan(0)
+	      
+        // Que espere hasta que llegue la respuesta 
+        done();
+      });
+    });
+
+
 });
