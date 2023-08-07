2fccbb9a328130bbef5fcb7b786d97a285c3180b
RicardoMolinaCoronel <ridumoli@espol.edu.ec>
2023-08-06 23:16:54 -0500

    Cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { Partido } from '../app/interfaces/partido';
-import { ServicioService } from '../app/providers/servicio.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,19 +7,5 @@ import { ServicioService } from '../app/providers/servicio.service';
 })
 export class AppComponent {
   title = 'internationalFootball';
-  public randomdata: Partido | null = null; // Inicializar como null o un objeto Partido
 
-  constructor(private dataProvider: ServicioService) { }
-
-  ngOnInit() {
-    this.generarPartidoAleatorio(); // Generar un partido aleatorio al cargar la página
-  }
-
-  generarPartidoAleatorio() {
-    this.dataProvider.getResponse().subscribe((response) => { 
-      const partidos: Partido[] = response as Partido[]; 
-      const indiceAleatorio = Math.floor(Math.random() * partidos.length);
-      this.randomdata = partidos[indiceAleatorio];
-    });
-  }
 }
--- src/app/calltoaction/calltoaction.component.ts
+++ src/app/calltoaction/calltoaction.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+//Importación de la interfaz
+import { Partido } from '../interfaces/partido';
 
+//Importación del servicio
+import { ServicioService } from '../providers/servicio.service';
 @Component({
   selector: 'app-calltoaction',
   templateUrl: './calltoaction.component.html',
   styleUrls: ['./calltoaction.component.css']
 })
 export class CalltoactionComponent {
+  public randomdata: Partido | null = null; // Inicializar como null o un objeto Partido
 
+  constructor(private dataProvider: ServicioService) { }
+
+  ngOnInit() {
+    this.generarPartidoAleatorio(); // Generar un partido aleatorio al cargar la página
+  }
+
+  generarPartidoAleatorio() {
+    this.dataProvider.getResponse().subscribe((response) => { 
+      const partidos: Partido[] = response as Partido[]; 
+      const indiceAleatorio = Math.floor(Math.random() * partidos.length);
+      this.randomdata = partidos[indiceAleatorio];
+    });
+  }
 }
